15075dba63132831b04e05f3dfd73f45aeedee61
monica.k.tran <monica.k.tran@gmail.com>
2017-03-30 23:54:47 -0500

    Made changes to dashboard layout

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,17 @@
+// Forces modal warnings to automatically popup on pageload.
 function modalPopup(){
     $("#modalWarning").modal("show");
 }
 jQuery(document).ready(function(){
     modalPopup();
+
+    $( ".product" ).change(function() {
+  		$(".color").removeAttr("disabled");
+	});
+	$( ".color" ).change(function() {
+  		$(".size").removeAttr("disabled");
+	});
+	$( ".size" ).change(function() {
+  		$(".quantity").removeAttr("disabled");
+	});
 });
