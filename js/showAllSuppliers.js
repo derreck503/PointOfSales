dd735330216a72a6abafbedaea59f3f295bbd01d
Derreck Stellpflug <derreck503@gmail.com>
2017-04-14 15:02:29 -0500

    working suppliers page, just need design improvements

--- js/showAllSuppliers.js
+++ js/showAllSuppliers.js
@@ -1,5 +1,5 @@
 function showAll() {
     console.log("Show All function entered");
-        document.getElementById("showSupplier").style.display = "none";
         document.getElementById("showAllSuppliers").style.display = "block";
+        document.getElementById("showSupplier").style.display = "none";
 }
--- js/showSuppliers.js
+++ js/showSuppliers.js
@@ -1,5 +1,5 @@
 function showSupplier() {
     console.log("Show supplier function entered");
-        document.getElementById("showAllSuppliers").style.display = "none"
+        document.getElementById("showAllSuppliers").style.display = "none";
         document.getElementById("showSupplier").style.display = "block";
 }
