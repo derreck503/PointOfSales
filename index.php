da502f26a17cd183954466d27bc7d61373dad5e7
Derreck Stellpflug <derreck503@gmail.com>
2017-04-15 14:10:40 -0500

    Updated current list of files for project

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
+<?php
+require 'database/connect.php';
+session_start();
+
+
+  if(isset($_POST["Log"])) {
+
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+
+
+   $sql = $db->query("SELECT Username, Password, UserID FROM POSDB.Login");
+    if($sql->num_rows){
+    $Logins = $sql->fetch_all(MYSQLI_ASSOC);
+    foreach($Logins as $Users){
+      if ($Users['Username'] == $username && $Users['Password'] == $password) {
+        $_SESSION['Identifier'] = $Users['UserID'];
+        header('Location: dashboard.php');
+        
+        }
+    }
+            echo "Invalid Username or Password";
+       
+  }
+
+
+  }
+
+?>
+
+
+  
+
 <!DOCTYPE html>
 <html>
 
@@ -20,11 +53,11 @@
   <div class="container" style="text-align: center; margin-top: 250px; width: 80%;">
     <!--Title banner-->
     <div class="jumbotron" style="background-color:aliceblue;">
-      <p>You have been logged out.</p>
-      <hr>
-      <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#popUpWindow">Log In</button>
+      <h1>Welcome to Babies R Us</h1>
+      <p>The leading point of sales system for a baby store.</p>
     </div>
     <!--Login button-->
+    <button type="button" class="btn btn-danger btn-lg" data-toggle="modal" data-target="#popUpWindow">Log In</button>
     <!--Contents of button-->
     <div class="modal fade" id="popUpWindow" style="margin-top: 300px;">
       <div class="modal-dialog">
@@ -36,18 +69,18 @@
           </div>
           <!--Body inside of button-->
           <div class="modal-body">
-            <form role="form">
+            <form role="form" action="" method="post">
               <div class="form-group">
-                <input type="email" class="form-control" placeholder="Email">
+                <input type="username" class="form-control" placeholder="Username" name='username'>
               </div>
               <div class="form-group">
-                <input type="password" class="form-control" placeholder="Password">
+                <input type="password" class="form-control" placeholder="Password" name='password'>
               </div>
-            </form>
-          </div>
+              </div>
           <!--Footer inside of button-->
           <div class="modal-footer">
-            <a href="dashboard.html"><button class="btn btn-primary btn-block">Log In</button></a>
+           <button class="btn btn-primary btn-block" type="submit" name="Log" >Log In</button>
+           </form>
           </div>
         </div>
       </div>
--- logout.php
+++ logout.php
@@ -1,3 +1,29 @@
+<?php
+require 'database/connect.php';
+
+
+  if(isset($_POST["Log"])) {
+
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+
+   $sql = $db->query("SELECT Username, Password FROM POSDB.Login");
+    if($sql->num_rows){
+    $Logins = $sql->fetch_all(MYSQLI_ASSOC);
+    foreach($Logins as $Users){
+      if ($Users['Username'] == $username && $Users['Password'] == $password) {
+        header('Location: dashboard.php');
+        }
+    }
+            echo "Invalid Username or Password";
+       
+  }
+  }
+
+?>
+
+
+
 <!DOCTYPE html>
 <html>
 
@@ -20,34 +46,34 @@
   <div class="container" style="text-align: center; margin-top: 250px; width: 80%;">
     <!--Title banner-->
     <div class="jumbotron" style="background-color:aliceblue;">
-      <h1>Welcome to Babies R Us</h1>
-      <p>The leading point of sales system for a baby store.</p>
+      <p>You have been logged out.</p>
+      <hr>
+      <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#popUpWindow">Log In</button>
     </div>
     <!--Login button-->
-    <button type="button" class="btn btn-danger btn-lg" data-toggle="modal" data-target="#popUpWindow">Log In</button>
     <!--Contents of button-->
     <div class="modal fade" id="popUpWindow" style="margin-top: 300px;">
       <div class="modal-dialog">
         <div class="modal-content">
           <!--Header inside of button-->
-          <div class="modal-header">
+                    <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal">&times;</button>
             <h3 class="modal-title">Log In</h3>
           </div>
           <!--Body inside of button-->
           <div class="modal-body">
-            <form role="form">
+            <form role="form" action="" method="post">
               <div class="form-group">
-                <input type="email" class="form-control" placeholder="Email">
+                <input type="username" class="form-control" placeholder="Username" name='username'>
               </div>
               <div class="form-group">
-                <input type="password" class="form-control" placeholder="Password">
+                <input type="password" class="form-control" placeholder="Password" name='password'>
               </div>
-            </form>
-          </div>
+              </div>
           <!--Footer inside of button-->
           <div class="modal-footer">
-            <a href="dashboard.html"><button class="btn btn-primary btn-block">Log In</button></a>
+           <button class="btn btn-primary btn-block" type="submit" name="Log" >Log In</button>
+           </form>
           </div>
         </div>
       </div>
@@ -57,3 +83,5 @@
 </body>
 
 </html>
+
+
