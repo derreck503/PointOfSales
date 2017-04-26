5a94edae700e409a6d3e1bcae00e326705aba4b0
Derreck Stellpflug <derreck503@gmail.com>
2017-04-25 22:56:35 -0500

    fixed show triggers page

--- js/showTriggers.js
+++ js/showTriggers.js
@@ -2,5 +2,5 @@ function showAllTrigger() {
     console.log("Show triggers function entered");
         document.getElementById("showAllProducts").style.display = "none";
         document.getElementById("showProduct").style.display = "none";
-                document.getElementById("showAllTriggers").style.display = "block";
+        document.getElementById("showAllTriggers").style.display = "block";
 }
